97ea97ea0fccb856e16547a2b0041b2594601e8f
adamkosinar <adam.kosinar@venatusmedia.com>
2021-11-25 13:59:04 +0000

    added screenshot

--- index.ts
+++ index.ts
@@ -5,7 +5,8 @@ import {HttpClient} from "./src/HttpClient/HttpClient";
 const urls = [
     "https://ft-tech-test-example.s3-eu-west-1.amazonaws.com/ftse-fsi.json",
     "https://ft-tech-test-example.s3-eu-west-1.amazonaws.com/gbp-hkd.json",
-    "https://ft-tech-test-example.s3-eu-west-1.amazonaws.com/gbp-usd.json"
+    "https://ft-tech-test-example.s3-eu-west-1.amazonaws.com/gbp-usd.json",
+    "https://ft-tech-test-example.s3-eu-west-1.amazonaws.com/nope"
 ];
 
 const client = container.resolve(HttpClient);
